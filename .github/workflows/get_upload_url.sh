2d205ae91583234a4d75b4b2e115091c2575c153
adeutou <a.deutou@gmail.com>
2023-08-24 20:13:38 +0200

    get_upload_url script

--- .github/workflows/get_upload_url.sh
+++ .github/workflows/get_upload_url.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-upload_url=$(curl -sS -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" -X GET "https://api.github.com/repos/${{ github.repository }}/releases/${{ github.run_number }}" | jq -r '.upload_url' | sed 's/{?name,label}//')
+upload_url=$(curl -sS -H "Authorization: Bearer ${{ secrets.TOKEN }}" -X GET "https://api.github.com/repos/${{ github.repository }}/releases/${{ github.run_number }}" | jq -r '.upload_url' | sed 's/{?name,label}//')
 echo "::set-output name=upload_url::$upload_url"
